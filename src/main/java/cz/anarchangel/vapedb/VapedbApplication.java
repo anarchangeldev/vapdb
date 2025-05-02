00a7d9225adb5d1fd205e5b1bb3d99f94b00bfe2
lpetracek <lukas.petracek@media-sol.com>
2025-05-02 20:24:13 +0200

    Initial commit

---
+++ src/main/java/cz/anarchangel/vapedb/VapedbApplication.java
@@ -0,0 +1,13 @@
+package cz.anarchangel.vapedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VapedbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VapedbApplication.class, args);
+	}
+
+}
